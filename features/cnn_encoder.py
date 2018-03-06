dd3e14c7833754439b27a6595b7cb292098f68ca
xinyandai <381675152@qq.com>
2018-03-06 10:31:51 +0800

    Dev: features learning with auto coder.

--- features/cnn_encoder.py
+++ features/cnn_encoder.py
@@ -20,23 +20,22 @@ class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
 
-        self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
-            nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
-        )
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
-            nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
-            nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
-            nn.Tanh()
-        )
-
+        # self.encoder = nn.Sequential(
+        #     nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+        #     nn.ReLU(True),
+        #     nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+        #     nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
+        #     nn.ReLU(True),
+        #     nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        # )
+        # self.decoder = nn.Sequential(
+        #     nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+        #     nn.ReLU(True),
+        #     nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+        #     nn.ReLU(True),
+        #     nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+        #     nn.Tanh()
+        # )
 
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
--- images/loader.py
+++ images/loader.py
@@ -1,6 +1,7 @@
 import torch.utils.data as data
 from PIL import Image
 import os
+import numpy as np
 
 
 def is_image_file(filename):
@@ -9,6 +10,48 @@ def is_image_file(filename):
 
 def load_img(file_path):
     return Image.open(file_path).convert('RGB')
+
+
+def fvecs_read(filename, c_contiguous=True):
+    fv = np.fromfile(filename, dtype=np.float32)
+
+    if fv.size == 0:
+        return np.zeros((0, 0))
+
+    dim = fv.view(np.int32)[0]
+    assert dim > 0
+
+    fv = fv.reshape(-1, 1 + dim)
+
+    if not all(fv.view(np.int32)[:, 0] == dim):
+        raise IOError("Non-uniform vector sizes in " + filename)
+
+    fv = fv[:, 1:]
+    if c_contiguous:
+        fv = fv.copy()
+
+    return fv
+
+
+class FvecsDataSet(data.Dataset):
+    def __init__(self, fvecs_file, transform=None, target_transform=None):
+        super(FvecsDataSet, self).__init__()
+
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.features = fvecs_read(fvecs_file)
+
+    def __getitem__(self, index):
+        input_feature = self.features[index]
+        if self.transform is not None:
+            input_feature = self.transform(input_feature)
+        if self.target_transform is not None:
+            index = self.target_transform(index)
+        return input_feature, index
+
+    def __len__(self):
+        return len(self.features)
 
 
 class SingleFolderDataSet(data.Dataset):
